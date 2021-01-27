ecfe2dd3831a21231496f8d064a64962fe1c3579
Hersain Lozada Hermosa <hersainlozada@gmail.com>
2021-01-26 19:05:30 -0500

    operadores_asignacion_26_01_2021

---
+++ main.js
@@ -0,0 +1,70 @@
+
+//Operadores_asignacion_26_01_2021
+let x = 10;
+let y = 20;
+
+console.log('Variable x = ',x);
+console.log('Variable y = ',y);
+//Asignacion
+x = y
+console.log('Asignación: x = y = ',x);
+
+//Adicion
+x = 10;
+x = x+y;    //Abreviación: x += y
+console.log('Asignación Adición: x+y = ',x);
+
+//Sustracción
+x = 10;
+x = x-y;    //Abreviación: x -= y
+console.log('Asignación Sustracción: x-y = ',x);
+
+//Multiplicación
+x = 10;
+x = x*y;    //Abreviación: x *= y
+console.log('Asignación Multiplicación: x*y = ',x);
+
+//Division
+x = 10;
+x = x/y;    //Abreviación: x /= y
+console.log('Asignación División: x/y = ',x);
+
+//Resto
+x = 10;
+x = x%y;    //Abreviación: x %= y
+console.log('Asignación Resto: x%y = ',x);
+
+//Exponenciacion
+x = 10;
+x = x**y;    //Abreviación: x **= y
+console.log('Asignación Exponenciacón: x**y = ',x);
+
+//Desplazamiento a la izquierda
+x = 10;
+x = x<<y;    //Abreviación: x <<= y
+console.log('Asignación Desplazamiento a la izquierda: x<<y = ',x);
+
+//Desplazamiento a la derecha
+x = 10;
+x = x>>y;    //Abreviación: x >>= y
+console.log('Asignación Desplazamiento a la derecha: x>>y = ',x);
+
+//Asignación sin signo de Desplazamiento a la derecha
+x = 10;
+x = x>>>y;    //Abreviación: x >>>= y
+console.log('Asignación sin signo de Desplazamiento a la derecha: x>>>y = ',x);
+
+//Asignación AND
+x = 10;
+x = x&y;    //Abreviación: x &= y
+console.log('Asignación AND: x&y = ',x);
+
+//Asignación XOR
+x = 10;
+x = x^y;    //Abreviación: x ^= y
+console.log('Asignación XOR: x^y = ',x);
+
+//Asignación OR
+x = 10;
+x = x|y;    //Abreviación: x |= y
+console.log('Asignación OR: x|y = ',x);
